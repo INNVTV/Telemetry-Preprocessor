21f59c3930a3f715de2f6a92e34c1263ec5029b1
Kaz <kaz@innvtv.com>
2020-01-03 12:17:42 -0800

    Implemented exponential back-off and started shared constants project

---
+++ Shared/Common/Constants/QueueNames.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Common.Constants
+{
+    /*
+     *  Queue Naming Rules
+     * 
+     *  1. A queue name must start with a letter or number, and can only contain letters, numbers, and the dash (-) character.
+     *  2. The first and last letters in the queue name must be alphanumeric. The dash (-) character cannot be the first or last character. Consecutive dash characters are not permitted in the queue name.
+     *  3. All letters in a queue name must be lowercase.
+     *  4. A queue name must be from 3 through 63 characters long.
+     *  
+     */
+
+    public static class QueueNames
+    {
+        public readonly static string MainRecordTask = "content-views-update-main-record";
+        public readonly static string DataLakeTask = "content-views-update-data-lake";
+        public readonly static string CountTableTask = "content-views-update-count-table";
+        public readonly static string ReportsTableTask = "content-views-update-report-table";
+    }
+}
--- TaskWorkers/UpdateMainRecordTask/Worker.cs
+++ TaskWorkers/UpdateMainRecordTask/Worker.cs
@@ -12,17 +12,41 @@ namespace UpdateMainRecordTask
     {
         private readonly ILogger<Worker> _logger;
 
+        // For exponential back-off
+        private int minInterval;
+        private int interval;
+        private int exponent;
+        private int maxInterval;
+
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+
+            minInterval = 1;
+            interval = minInterval;
+            exponent = 2;
+            maxInterval = 90;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                //var message = queue.GetMessage(); if (message != null)
+                //{
+
+
+                    //interval = minInterval;
+                    //_logger.LogInformation($"Interval reset to {interval}");
+                //}
+                //else
+                //{
+                    _logger.LogInformation($"Sleep for {interval} seconds");
+                    await Task.Delay((interval * 1000), stoppingToken);
+
+                    interval = Math.Min(maxInterval, interval * exponent);
+
+                //}
             }
         }
     }
