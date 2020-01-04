36e3cdc8c9d964fe5d3cf3a04a3ffe15809e1dcc
Kaz <kaz@innvtv.com>
2020-01-03 16:07:13 -0800

    Added message queue nuget package

--- TaskWorkers/UpdateMainRecordTask/Worker.cs
+++ TaskWorkers/UpdateMainRecordTask/Worker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Storage.Queues;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -24,29 +25,37 @@ namespace UpdateMainRecordTask
 
             minInterval = 1;
             interval = minInterval;
-            exponent = 2;
+            exponent = 3;
             maxInterval = 90;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var connectionString = "";
+            var queueName = "";
+            QueueClient queue = new QueueClient(connectionString, queueName);
+
             while (!stoppingToken.IsCancellationRequested)
             {
-                //var message = queue.GetMessage(); if (message != null)
-                //{
+                var messages = await queue.ReceiveMessagesAsync(maxMessages:1);
+                var message = messages.Value.FirstOrDefault();
+                
+                if (message != null)
+                {
+                    // Run Task(s)
 
-
-                    //interval = minInterval;
-                    //_logger.LogInformation($"Interval reset to {interval}");
-                //}
-                //else
-                //{
+                    // Delete message and reset interval
+                    await queue.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+                    interval = minInterval;
+                    _logger.LogInformation($"Interval reset to {interval}");
+                }
+                else
+                {
                     _logger.LogInformation($"Sleep for {interval} seconds");
                     await Task.Delay((interval * 1000), stoppingToken);
 
                     interval = Math.Min(maxInterval, interval * exponent);
-
-                //}
+                }
             }
         }
     }
