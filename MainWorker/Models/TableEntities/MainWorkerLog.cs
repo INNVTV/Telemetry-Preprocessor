c481fe2e7a60c4bf6ff94f13847392a94be1973a
Kaz <kaz@innvtv.com>
2020-01-04 18:59:23 -0800

    Added table entities

---
+++ MainWorker/Models/TableEntities/MainWorkerLog.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainWorker.Models.TableEntities
+{
+    public class MainWorkerLog : TableEntity
+    {
+        MainWorkerLog(string temporalId)
+        {
+            PartitionKey = temporalId;
+            RowKey = string.Format("{0:d19}+{1}", DateTime.MaxValue.Ticks - DateTime.UtcNow.Ticks, Guid.NewGuid().ToString("N"));
+
+            RecordsProcessed = 0;
+            DateProcessed = DateTime.UtcNow;
+        }
+
+        public string TemporalId
+        {
+            get { return PartitionKey; }
+            set { PartitionKey = value; }
+        }
+
+        public int RecordsProcessed { get; set; }
+        public DateTime DateProcessed { get; set; }
+
+    }
+}
